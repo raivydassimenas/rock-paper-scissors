01f620de16e09b3f3a6782c86a2784be04f7dd78
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-09 19:40:00 +0300

    Half done introducing DOM functionality

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,6 +5,64 @@ function getComputerChoice() {
     return choices[compChoice];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+document.querySelector('.player-score').innerText = playerScore;
+document.querySelector('.computer-score').innerText = computerScore;
+
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener('click', function(e) {
+    const outcome = playRound('rock', getComputerChoice());
+    const result = document.createElement('p');
+    result.innerText = outcome;
+    document.querySelector('.results').appendChild(result);
+
+    if (outcome.startsWith('You win')) {
+        playerScore++;
+        document.querySelector('.player-score').innerText = playerScore;
+    } else if (outcome.startsWith('You lose')) {
+        computerScore++;
+        document.querySelector('.computer-score').innerText = computerScore;
+    }
+
+    
+});
+
+paper.addEventListener('click', function(e) {
+    const outcome = playRound('paper', getComputerChoice());
+    const result = document.createElement('p');
+    result.innerText = outcome;
+    document.querySelector('.results').appendChild(result);
+    
+    if (outcome.startsWith('You win')) {
+        playerScore++;
+        document.querySelector('.player-score').innerText = playerScore;
+    } else if (outcome.startsWith('You lose')) {
+        computerScore++;
+        document.querySelector('.computer-score').innerText = computerScore;
+    }
+});
+
+scissors.addEventListener('click', function(e) {
+    const outcome = playRound('scissors', getComputerChoice());
+    const result = document.createElement('p');
+    result.innerText = outcome;
+    document.querySelector('.results').appendChild(result);
+
+    if (outcome.startsWith('You win')) {
+        playerScore++;
+        document.querySelector('.player-score').innerText = playerScore;
+    } else if (outcome.startsWith('You lose')) {
+        computerScore++;
+        document.querySelector('.computer-score').innerText = computerScore;
+    }
+});
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
@@ -54,4 +112,4 @@ function game() {
     }
 }
 
-game();
+// game();
